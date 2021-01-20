a70479a3c2be458197cc561270aa58b543a6b3a8
Beshen <56017939+Beshen20@users.noreply.github.com>
2021-01-20 17:03:13 +0200

    Add files via upload

--- 13/js/script.js
+++ 13/js/script.js
@@ -1,74 +1,84 @@
-const Hamburger = {
+class Products {
+  constructor() {
+    this.cheese = [10, 20];
+    this.salad = [20, 5];
+    this.potato = [15, 10];
+    this.seasoning = [15, 0];
+    this.mayonnaise = [20, 5];
+  }
 
-  price: 0,
-  calories: 0,
+  switchSize(size) {
+    const [small, average, large] = [
+      [50, 20],
+      [75, 30],
+      [100, 40]
+    ];
+    const sizes = {
+      small,
+      average,
+      large
+    };
+    const [price, calories] = sizes[size];
+    return {
+      price,
+      calories
+    };
+  }
 
-  small: {
-    price: 50,
-    calories: 20
-  },
-  average: {
-    price: 75,
-    calories: 30
-  },
-  large: {
-    price: 100,
-    calories: 40
-  },
+  getProduct(name) {
+    const [price, calories] = this[name];
+    return {
+      price,
+      calories
+    };
+  }
+}
 
-  cheese: {
-    price: 10,
-    calories: 20
-  },
+class Hamburger {
+  constructor() {
+    this.price = 0;
+    this.colories = 0;
+    this.products = new Products();
+  }
 
-  salad: {
-    price: 20,
-    calories: 5
-  },
+  result(price, calories) {
+    document.getElementById('price').innerHTML = price;
+    document.getElementById('calories').innerHTML = calories;
+  }
 
-  potato: {
-    price: 15,
-    calories: 10
-  },
-
-  seasoning: {
-    price: 15,
-    calories: 0
-  },
-
-  mayonnaise: {
-    price: 20,
-    calories: 5
-  },
-
-  calc: function (size) {
-    this.price += Hamburger[size].price;
-    this.calories += Hamburger[size].calories;
+  calc(size) {
+    const {
+      price,
+      calories
+    } = this.products.switchSize(size);
+    this.price += price;
+    this.calories += calories;
 
     let checkedBoxes = document.querySelectorAll('input[name=add]:checked');
     for (let i = 0; i < checkedBoxes.length; i++) {
       let add = checkedBoxes[i].id;
-      this.price += Hamburger[add].price;
-      this.calories += Hamburger[add].calories;
+      const {
+        price,
+        calories
+      } = this.products.getProduct(add);
+
+      this.price += price;
+      this.calories += calories;
     }
 
-    Hamburger.result(this.price, this.calories);
-  },
+    this.result(this.price, this.calories);
+  }
 
-  result: function (price, calories) {
-    document.getElementById('price').innerHTML = price;
-    document.getElementById('calories').innerHTML = calories;
-  },
-
-  resetData: function () {
+  resetData() {
     this.price = 0;
     this.calories = 0;
-    Hamburger.result(this.price, this.calories);
+    this.result(this.price, this.calories);
   }
-};
+}
 
 document.getElementById('form').addEventListener('submit', function (e) {
-  Hamburger.resetData();
-  Hamburger.calc(document.querySelector('input[name="size"]:checked').value);
+  const hamburger = new Hamburger(Products);
+  hamburger.resetData();
+  hamburger.calc(document.querySelector('input[name="size"]:checked').value);
   e.preventDefault();
 });
