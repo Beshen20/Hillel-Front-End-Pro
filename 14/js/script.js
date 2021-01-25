06abf01e3338ed3f3c0e7455fd9c75f0710b4cf7
Beshen <56017939+Beshen20@users.noreply.github.com>
2021-01-25 10:38:10 +0200

    Add files via upload

---
+++ 14/js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+class Group {
+  constructor() {
+    this._students = [];
+  }
+  get students() {
+    return this._students;
+  }
+  addStudent(student) {
+    this._students.push(student);
+  }
+  getAverageMark() {
+    let sum = 0;
+    let count = 0;
+    this._students.map(student => {
+      let studentMarkSum = 0;
+      let studentMarkCount = 0;
+      student.marks.map(mark => {
+        studentMarkSum += mark;
+        studentMarkCount += 1;
+      });
+      const studentAverage = studentMarkSum / studentMarkCount;
+      console.log(student.name + ' ' + studentAverage);
+      sum += studentAverage;
+      count += 1;
+    });
+    return sum / count;
+  }
+}
+class Student {
+  constructor(name, marks) {
+    this.name = '';
+    this.marks = [];
+    this.name = name;
+    this.marks = marks;
+  }
+}
+const feGroup = new Group();
+// const firstStudent = new Student('John Doe', [10, 102, 0]);
+feGroup.addStudent(new Student('John Doe', [10, 10, 5, 10]));
+feGroup.addStudent(new Student('Alex Smith', [10, 9, 8]));
+feGroup.addStudent(new Student('Bob Johnson', [9, 10, 10, 8]));
+// console.log(feGroup.students); // [{},{},{}]
+console.log(feGroup.getAverageMark());
+console.log(feGroup.students);
