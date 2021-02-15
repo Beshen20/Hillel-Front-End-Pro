b88cba4bfc14a013110084e197f15e15a11fbf07
Beshen <56017939+Beshen20@users.noreply.github.com>
2021-02-15 11:54:26 +0200

    Add files via upload

--- 19/js/script.js
+++ 19/js/script.js
@@ -3,25 +3,23 @@ const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?albumId={{id}}';
 
 const ALBUM_ITEM_CLASS = 'album-item';
 
-const albumsEl = document.querySelector('#albums');
-const photosEl = document.querySelector('#photos');
+const albumsEl = document.querySelector('#album');
+const photosEl = document.querySelector('#photo');
 
-const albumItemTemplate = document.querySelector('#albumItemTemplate')
-    .innerHTML;
-const photoItemTemplate = document.querySelector('#photoItemTemplate')
-    .innerHTML;
+const albumItemTemplate = document.querySelector('#albumItem').innerHTML;
+const photoItemTemplate = document.querySelector('#photoItem').innerHTML;
 
-albumsEl.addEventListener('click', onAlbumsClick);
+albumsEl.addEventListener('click', onClick);
 
 init();
 
 function init() {
-    getAlbums().then(getFirstAlbumPhotos);
+    getAlbums().then(getFirstPhotos);
 }
 
 function getAlbums() {
     return fetch(ALBUMS_URL)
-        .then((resp) => resp.json())
+        .then((res) => res.json())
         .then((albumsList) => {
             renderAlbums(albumsList);
             return albumsList;
@@ -30,45 +28,45 @@ function getAlbums() {
 
 function renderAlbums(data) {
     albumsEl.innerHTML = data
-        .map((album) => generateAlbumHtml(album))
+        .map((album) => createAlbum(album))
         .join('\n');
 }
 
-function generateAlbumHtml(album) {
+function createAlbum(album) {
     return albumItemTemplate
         .replace('{{id}}', album.id)
         .replace('{{title}}', album.title);
 }
 
-function getFirstAlbumPhotos(data) {
+function getFirstPhotos(data) {
     if (data.length) {
         getPhotos(data[0].id);
     }
 }
 
 function getPhotos(albumId) {
-    return fetch(getPhotosUrl(albumId))
+    return fetch(getPhotoUrl(albumId))
         .then((resp) => resp.json())
         .then(renderPhotos);
 }
 
-function getPhotosUrl(albumId) {
+function getPhotoUrl(albumId) {
     return PHOTOS_URL.replace('{{id}}', albumId);
 }
 
 function renderPhotos(data) {
     photosEl.innerHTML = data
-        .map((photo) => generatePhotoHtml(photo))
+        .map((photo) => createPhoto(photo))
         .join('\n');
 }
 
-function generatePhotoHtml(photo) {
+function createPhoto(photo) {
     return photoItemTemplate
         .replace('{{url}}', photo.thumbnailUrl)
         .replace('{{title}}', photo.title);
 }
 
-function onAlbumsClick(e) {
+function onClick(e) {
     if (e.target.classList.contains(ALBUM_ITEM_CLASS)) {
         getPhotos(e.target.dataset.id);
     }
